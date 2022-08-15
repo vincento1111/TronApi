0885e9dab5cf7b699912f3348cd796aad06fa4af
Vincento1111 <vincento2009@hotmail.com>
2022-08-15 02:38:10 +0200

    Added new folder for classes containing information on tables in tronapidb

--- TronApi/Controllers/UserAccountController.cs
+++ TronApi/Controllers/UserAccountController.cs
@@ -21,6 +21,15 @@ namespace TronApi.Controllers
         }
 
 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<UserAccount>>> Get(int id)
+        {
+            var user = await _context.userAccounts.FindAsync(id);
+            if (user == null)
+                return BadRequest("404 user not found");
+            return Ok(user);
+        }
+
         [HttpPost]
         public async Task<ActionResult<List<UserAccount>>> AddUserAccount(UserAccount userAccount)
         {
@@ -30,6 +39,33 @@ namespace TronApi.Controllers
             return Ok(await _context.userAccounts.ToListAsync());
         }
 
+        [HttpPut]
 
+        public async Task<ActionResult<List<UserAccount>>> UpdateUserAccount(UserAccount Request)
+        {
+            var dbUser = await _context.userAccounts.FindAsync(Request.Id);
+            if (dbUser == null)
+                return NotFound("Hero not found");
+
+            dbUser.Email = Request.Email;
+            dbUser.Password = Request.Password;
+
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.userAccounts.ToListAsync());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<UserAccount>>> Delete(int id)
+        {
+            var dbUser = await _context.userAccounts.FindAsync(id);
+            if (dbUser == null)
+                return BadRequest("404 hero not found");
+
+            _context.userAccounts.Remove(dbUser);
+            await _context.SaveChangesAsync();
+            return Ok(await _context.userAccounts.ToListAsync());
+        }
     }
 }
